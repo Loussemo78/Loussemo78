b36f48e36b1cc9576b52c9f281a24a7847dfbc07
michael <michael.mfoundou@yahoo.fr>
2022-03-30 13:54:52 +0200

    view model completed

--- app/src/main/java/com/example/marmeladetest/view/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/example/marmeladetest/view/viewmodel/QuoteViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.marmeladetest.view.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
 import com.example.marmeladetest.repository.QuoteRepository
 import com.example.marmeladetest.view.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
@@ -18,4 +22,15 @@ class QuoteViewModel @Inject constructor(
     private val _quoteList by lazy { MutableLiveData<ViewState<Response<QuoteListQuery.Data>>>() }
     val charactersList: LiveData<ViewState<Response<QuoteListQuery.Data>>>
         get() = _quoteList
+
+    fun queryCharactersList() = viewModelScope.launch {
+        _quoteList.postValue(ViewState.Loading())
+        try {
+            val response = repository.queryQuoteList()
+            _quoteList.postValue(ViewState.Success(response))
+        } catch (e: ApolloException) {
+            Log.d("ApolloException", "Failure", e)
+            _quoteList.postValue(ViewState.Error("Error fetching characters"))
+        }
+    }
 }
